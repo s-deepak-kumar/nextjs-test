6b90fac9eb4004e46d4c385c23c63540d4eb14b3
S Deepak Kumar <dipkfilms@gmail.com>
2025-11-22 18:57:32 +0530

    error added

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export function handleRequest(request) {
-  // Example optimized code to reduce recursive operations and complex computations
-  const url = new URL(request.url);
-  const response = await fetch(url.href);
-  return response;
-}
